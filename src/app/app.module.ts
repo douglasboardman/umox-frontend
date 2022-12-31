7543ee8bdaf52a67f6ea589f52af0381aea83f4e
Douglas Boardman <46564691+douglasboardman@users.noreply.github.com>
2022-12-31 14:11:39 -0300

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { ContentComponent } from './components/content/content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    DashboardComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule,
