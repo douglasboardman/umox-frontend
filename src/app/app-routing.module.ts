4230b2e75adbbb49fef21352b080dafe3e612e58
Douglas Boardman <46564691+douglasboardman@users.noreply.github.com>
2023-01-01 17:39:42 -0300

    Criação do formulário de login e Registro

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { RegisterPageComponent } from './pages/register-page/register-page.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'dashboard', pathMatch: 'full'},
+  { path: '', redirectTo: 'register', pathMatch: 'full'},
+  { path: 'login', component: LoginPageComponent},
+  { path: 'register', component: RegisterPageComponent},
   { path: 'dashboard', component: DashboardComponent}
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,19 @@ import { AppComponent } from './app.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { ContentComponent } from './components/content/content.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { TopbarComponent } from './components/topbar/topbar.component';
+import { RegisterPageComponent } from './pages/register-page/register-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SidebarComponent,
     DashboardComponent,
-    ContentComponent
+    ContentComponent,
+    LoginPageComponent,
+    TopbarComponent,
+    RegisterPageComponent
   ],
   imports: [
     BrowserModule,
