f1e975d377996864911833650a998cb70cc22265
Douglas Boardman <46564691+douglasboardman@users.noreply.github.com>
2023-04-04 14:22:12 -0300

    Commit

--- src/app/services/web-request.service.ts
+++ src/app/services/web-request.service.ts
@@ -8,7 +8,7 @@ export class WebRequestService {
   readonly ROOT_URL;
 
   constructor(private http: HttpClient) {
-    this.ROOT_URL = 'http://192.168.10.100:3000';
+    this.ROOT_URL = 'http://192.168.10.20:3001';
   }
 
   get(uri: string) {
